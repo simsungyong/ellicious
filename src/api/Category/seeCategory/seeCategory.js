b4ed2331f665cd1e412c991a6424c132be3215dc
Sim sungyong <ssy332972@gmail.com>
2019-11-15 17:34:37 +0900

    push

--- src/api/Category/seeCategory/seeCategory.js
+++ src/api/Category/seeCategory/seeCategory.js
@@ -3,7 +3,7 @@ import { isAuthenticated } from "../../../middlewares";
 
 export default {
     Query: {
-        seeCategory: async(_, {request})=>{
+        seeCategory: async(_, __,{request})=>{
             isAuthenticated(request);
             const {user} = request;
             return prisma.categories({where:{
