312063725aa3a812af05bb26c285a6bdb4276491
Sim sungyong <ssy332972@gmail.com>
2019-11-17 23:29:24 +0900

    d

--- src/api/Message/Message.js
+++ src/api/Message/Message.js
@@ -3,7 +3,8 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     Message: {
         room:({id})=>prisma.message({id}).room(),
-        user:({id})=>prisma.message({id}).user()
+        from:({id})=>prisma.message({id}).user(),
+        to:({id})=>prisma.message({id}).user()
     }
     
 }
