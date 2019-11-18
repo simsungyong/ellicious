ff4028b0b9492a84b39e9554f3436de9192ee19c
Sim sungyong <ssy332972@gmail.com>
2019-11-18 15:46:15 +0900

    ff

--- src/api/Category/Category.js
+++ src/api/Category/Category.js
@@ -3,6 +3,6 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     Category: {
         user: ({id})=>prisma.category({id}).user(),
-        posts: ({id})=>prisma.category({id}).posts()
+        posts: ({id})=>prisma.posts({where:{category:{id}}})
     }
 }
