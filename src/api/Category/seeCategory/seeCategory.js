531764fe717ef6cd636ad8d631f500fcfa7c1d78
Sim sungyong <ssy332972@gmail.com>
2019-12-05 16:12:23 +0900

    f

--- src/api/Category/seeCategory/seeCategory.js
+++ src/api/Category/seeCategory/seeCategory.js
@@ -6,11 +6,22 @@ export default {
         seeCategory: async(_, args,{request})=>{
             isAuthenticated(request);
             const {userId} = args;
+            const {user} = request;
+
+            if(!userId){
+                return prisma.categories({where:{
+                    user:{
+                        id:user.id
+                    }
+                }})
+            }
+            else{
             return prisma.categories({where:{
                 user:{
                     id:userId
                 }
             }});
         }
+    }
     }
 }
