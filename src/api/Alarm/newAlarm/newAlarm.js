75238157db08595093475994a55c81d2160c389f
Sim sungyong <ssy332972@gmail.com>
2020-02-14 19:41:13 +0900

    newAlarm

---
+++ src/api/Alarm/newAlarm/newAlarm.js
@@ -0,0 +1,24 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+export default {
+    Subscription : {
+        newAlarm:{
+            subscribe: (_,__,{request})=>{
+            isAuthenticated(request);
+            const {user} = request;
+            //console.log(user.id)
+            return prisma.$subscribe.alarm({
+                AND:[
+                    {mutation_in:"CREATED"},
+                    {
+                        node: {
+                            to : user.id
+                        }
+                    }
+                ]
+            }).node();
+        },
+        resolve: payload=>payload
+    }
+}
+}
