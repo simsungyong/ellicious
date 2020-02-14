f04ee0923a0689b5cd51a6e89f64719fc6e9184c
Sim sungyong <ssy332972@gmail.com>
2020-02-14 22:33:55 +0900

    f

--- src/api/Alarm/Alarm.js
+++ src/api/Alarm/Alarm.js
@@ -1,7 +1,7 @@
 import { prisma } from "../../../generated/prisma-client";
 export default {
     Alarm: {
-        from:({id})=> prisma.alarm({id}).user(),
+        from:({id})=> prisma.alarm({id}).from(),
         post:({id})=> prisma.alarm({id}).post()
 
     }
