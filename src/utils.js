9a8f527aed9a21592d886df86b69f085f6ee1e30
hongjoonki <ghdjk2216@naver.com>
2020-02-18 17:57:28 +0900

    c

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,23 @@
-import {adjectives, nouns} from './words';
+import { adjectives, nouns } from './words';
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({path: path.resolve(__dirname, '.env')});
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 import nodemailer from 'nodemailer';
 import sgTransport from 'nodemailer-sendgrid-transport';
 import jwt from 'jsonwebtoken';
+import aligoapi from 'aligoapi';
+import { SMS } from 'aws-sdk';
 
 
-
-export const generateSecretCode = ()=>{
-    const randomNum = Math.floor(Math.random()*nouns.length);
+export const generateSecretCode = () => {
+    const randomNum = Math.floor(Math.random() * nouns.length);
     return `${adjectives[randomNum]} ${nouns[randomNum]}`;
 }
 
 
-export const sendMail=(email)=>{
+export const sendMail = (email) => {
     const options = {
-        auth:{
+        auth: {
             api_user: process.env.SENDGRID_USERNAME,
             api_key: process.env.SENDGRID_PASSWORD
         }
@@ -25,8 +26,8 @@ export const sendMail=(email)=>{
     return client.sendMail(email);
 }
 
-export const sendSecretMail=(emailAddress, loginSecret)=>{ 
-    const email= {
+export const sendSecretMail = (emailAddress, loginSecret) => {
+    const email = {
         from: "Ellicious",
         to: emailAddress,
         subject: "Ellicious에 오신걸 환영합니다.",
@@ -35,4 +36,29 @@ export const sendSecretMail=(emailAddress, loginSecret)=>{
     return sendMail(email);
 }
 
-export const generateToken =(id)=>jwt.sign({id}, process.env.JWT_SECRET);  //토큰생성!! id가 파라미터
+export const sendSMS = (SMS) => {
+    const AuthData = {
+        key: process.env.ALIGO_SMS_API_KEY,
+        user_id: process.env.ALIGO_SMS_USER_ID
+    }
+
+    return aligoapi.send(SMS, AuthData)
+}
+
+export const sendSecretSMS = (phoneNumber, secretNumber) => {
+    const SMS = {
+        sender: "01025371907",
+        receiver: phoneNumber,
+        msg: `hihi! ${secretNumber}`,
+        msg_type: SMS,
+        title: "Ellicious",
+        // destination: % 고객명 % 치환용 입력
+        // rdate: 예약일(현재일이상) // YYYYMMDD
+        // rtime: 예약시간 - 현재시간기준 10분이후 // HHMM
+        // image: 첨부이미지 // JPEG, PNG, GIF
+    }
+
+    return sendMail(SMS)
+}
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);  //토큰생성!! id가 파라미터
