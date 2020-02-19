503c8fc46adc70914ae0927ccc128c9085fd0407
hongjoonki <ghdjk2216@naver.com>
2020-02-19 21:05:38 +0900

    c

--- src/upload.js
+++ src/upload.js
@@ -2,7 +2,6 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
 import './env';
-// import * as s3Storage from "multer-sharp-s3";
 
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_BUCKET,
@@ -25,27 +24,6 @@ const upload = multer({
     }
   })
 });
-
-// aws.config.update({
-//   secretAccessKey: process.env.AWS_BUCKET_SECRET,
-//   accessKeyId: process.env.AWS_BUCKET,
-//   region: "ap-northeast-2"
-// })
-// const s3 = new aws.S3()
-
-// const storage = s3Storage({
-//   s3,
-//   Bucket: "elliciouscontainer",
-//   Key: function(req, file, cb) {
-//     cb(null, Date.now().toString()+".jpg");
-//   },
-//   ACL: "public-read",
-//   resize: {
-//     width: 200,
-//     height: 200,
-//   }
-// })
-// const upload = multer({ storage: storage })
 
 
 export const uploadMiddleware = upload.array("file",10);
