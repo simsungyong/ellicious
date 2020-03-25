24a33e4d18c931858766e7714cce05eb42c26706
Sim sungyong <ssy332972@gmail.com>
2020-03-25 20:33:04 +0900

    f

--- src/api/User/recommendUser/recommendUser.js
+++ src/api/User/recommendUser/recommendUser.js
@@ -1,12 +1,9 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { isAuthenticated } from "../../../middlewares";
 
 export default {
     Query:{
         recommendUser: async(_,args)=>{
-            isAuthenticated(request);
-            //const { user } = request;
-            return await prisma.users({
+            return prisma.users({
                 first: args.items,
                 skip: args.pageNumber,
             });
